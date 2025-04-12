5f846fb032c0400402457c7135a515839556965d
Rishab raj <rishabraj2211@gmail.com>
2025-04-12 21:16:31 +0530

    defined entities and classes to be used

---
+++ app/src/main/java/ticket/booking/entities/Train.java
@@ -0,0 +1,14 @@
+package ticket.booking.entities;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class Train {
+
+    private  String trainId;
+    private String trainNum;
+    private List<List<Integer>> seats;
+    private Map<String, Date> stationTimes;
+    private List<String> stations;
+}
